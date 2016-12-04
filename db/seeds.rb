0a1c734c63c9506920087e0c2b0ef2fc33c982b4
Tim den Ouden <tachgus@gmail.com>
2016-12-04 21:18:16 +0100

    Deposit Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+# . . .
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description:
+                    %{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+                image_url: 'ruby.jpg',
+                price: 49.95)
+Product.create!(title: 'Programming PHP',
+                description:
+                    %{<p>
+PHP is also cool
+</p>},
+                image_url: 'php.jpg',
+                price: 46.95)
+# . . .
