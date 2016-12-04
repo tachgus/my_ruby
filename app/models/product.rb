124a138b28c6396758decb8f44f60f1b610a34c1
Tim den Ouden <tachgus@gmail.com>
2016-12-04 23:53:04 +0100

    Custom error message and validation on min length of 10 for title

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, length: {minimum: 10, message: 'Titel moet minstens 10 tekens zijn.'}
 end
