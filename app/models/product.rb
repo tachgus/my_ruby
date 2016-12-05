d9fcd51bd26c2662533d22638e4b7df17dd8d995
Tim den Ouden <tachgus@gmail.com>
2016-12-05 15:11:11 +0100

    Added shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -8,7 +10,22 @@ class Product < ApplicationRecord
   }
   validates :title, length: {minimum: 10, message: 'Titel moet minstens 10 tekens zijn.'}
 
+  # do not remove if refereced!
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   def self.latest
     Product.order(:updated_at).last
   end
+
+  # ensure that there are no line items referencing this product
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
